ce90cac772560b83316c81f9f6b5f6a5f59452dd
Jack (Pixegami) <pixegami@gmail.com>
2023-08-27 21:38:53 +1000

    patch: clear out CDK file

--- lib/discord-bot-lambda-stack.ts
+++ lib/discord-bot-lambda-stack.ts
@@ -1,16 +1,8 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class DiscordBotLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DiscordBotLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
