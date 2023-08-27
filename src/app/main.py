91ff3d18c178b20a8d3c49b1f755034022b55400
Jack (Pixegami) <pixegami@gmail.com>
2023-08-27 21:12:26 +1000

    feat: add Python Flask app to act as Discord endpoint
    Create the Flask app in main.py which will serve as interaction end point for the Discord bot that we will host on AWS Lambda.

---
+++ src/app/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
+async def interactions():
+    print(f"👉 Request: {request.json}")
+    raw_request = request.json
+    return interact(raw_request)
+
+
+def interact(raw_request):
+    if raw_request["type"] == 1:  # PING
+        response_data = {"type": 1}  # PONG
+    else:
+        data = raw_request["data"]
+        command_name = data["name"]
+
+        if command_name == "hello":
+            message_content = "Hello there!"
+        elif command_name == "echo":
+            original_message = data["options"][0]["value"]
+            message_content = f"Echoing: {original_message}"
+
+        response_data = {
+            "type": 4,
+            "data": {"content": message_content},
+        }
+
+    return jsonify(response_data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
