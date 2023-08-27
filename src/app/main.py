c810e23696946b37fdf17fec28071a5cd567362c
Jack (Pixegami) <pixegami@gmail.com>
2023-08-27 21:26:23 +1000

    feat: add Discord signature verification
    Discord endpoints require you to reject invalid tokens as a security measure, so this will validate the token from the request.

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,8 @@
+import os
 from flask import Flask, jsonify, request
+from discord_interactions import verify_key_decorator
+
+DISCORD_PUBLIC_KEY = os.environ.get("DISCORD_PUBLIC_KEY")
 
 app = Flask(__name__)
 
@@ -10,6 +14,7 @@ async def interactions():
     return interact(raw_request)
 
 
+@verify_key_decorator(DISCORD_PUBLIC_KEY)
 def interact(raw_request):
     if raw_request["type"] == 1:  # PING
         response_data = {"type": 1}  # PONG
