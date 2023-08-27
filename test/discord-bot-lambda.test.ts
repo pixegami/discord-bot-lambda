587e19e10c6118593bc7f47b78ce4e823b0d1859
Jack (Pixegami) <pixegami@gmail.com>
2023-08-27 20:28:06 +1000

    Initial commit

---
+++ test/discord-bot-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DiscordBotLambda from '../lib/discord-bot-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/discord-bot-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DiscordBotLambda.DiscordBotLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
